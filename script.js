7cc13988953ee44027ffe29ae1d32b0c93af3679
webhostingcom <isuwoxipide77@gmail.com>
2026-08-21 23:26:00 +0100

    Add more images to the galleryImages array
    Expanded the galleryImages array to include additional images.

--- script.js
+++ script.js
@@ -722,13 +722,39 @@ document.addEventListener("DOMContentLoaded", () => {
        GALLERY
     ================================================= */
 
-    const galleryImages = [
+const galleryImages = [
 
-        "assets/gallery-1.jpg",
-        "assets/gallery-2.jpg",
-        "assets/gallery-3.jpg"
-
-    ];
+    "assets/gallery-1.jpg",
+    "assets/gallery-2.jpg",
+    "assets/gallery-3.jpg",
+    "assets/gallery-4.jpg",
+    "assets/gallery-5.jpg",
+    "assets/gallery-6.jpg",
+    "assets/gallery-7.jpg",
+    "assets/gallery-8.jpg",
+    "assets/gallery-9.jpg",
+    "assets/gallery-10.jpg",
+    "assets/gallery-11.jpg",
+    "assets/gallery-12.jpg",
+    "assets/gallery-13.jpg",
+    "assets/gallery-14.jpg",
+    "assets/gallery-15.jpg",
+    "assets/gallery-16.jpg",
+    "assets/gallery-17.jpg",
+    "assets/gallery-18.jpg",
+    "assets/gallery-19.jpg",
+    "assets/gallery-20.jpg",
+    "assets/gallery-21.jpg",
+    "assets/gallery-22.jpg",
+    "assets/gallery-23.jpg",
+    "assets/gallery-24.jpg",
+    "assets/gallery-25.jpg",
+    "assets/gallery-26.jpg",
+    "assets/gallery-27.jpg",
+    "assets/gallery-28.jpg",
+    "assets/gallery-29.jpg",
+    "assets/gallery-30.jpg"
+];
 
 
     let currentGallery = 0;
